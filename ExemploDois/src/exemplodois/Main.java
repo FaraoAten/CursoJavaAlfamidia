09bc7a3a7d30a024678ea59d760256512c77a3ef
FaraoAten <atenfernandes@gmail.com>
2022-05-18 11:20:54 -0300

    Exemplo inicial apresentando a linguagem #2.

---
+++ ExemploDois/src/exemplodois/Main.java
@@ -0,0 +1,9 @@
+package exemplodois;
+
+public class Main {
+    public static void main(String[] args) {
+        String saida;
+        saida = "Segundo Exemplo";
+        System.out.println(saida);
+    }    
+}
