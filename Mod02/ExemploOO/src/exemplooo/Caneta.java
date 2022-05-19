8770132116ec89bcd220dc11942fa56569182b06
FaraoAten <atenfernandes@gmail.com>
2022-05-19 18:06:10 -0300

    Primeira aula pratica do módulo 2, uma apresentação a estrutura de O.O.

---
+++ Mod02/ExemploOO/src/exemplooo/Caneta.java
@@ -0,0 +1,17 @@
+package exemplooo;
+
+public class Caneta {
+
+    //Classe para representar um objeto caneta e suas funcionalidades.
+    
+    //Características da caneta.
+    //Representadas por variáveis.
+    private int cor;
+    private String tamanhoPonta;
+
+    //Funcionalidades da caneta.
+    //Representadas por métodos.
+    public void escrever() {
+        System.out.println("Palavras...");
+    }
+}
---
+++ Mod02/ExemploOO/src/exemplooo/Main.java
@@ -0,0 +1,10 @@
+package exemplooo;
+
+public class Main {
+    public static void main(String[] args){
+        
+        Caneta caneta = new Caneta(); //Criando o objeto.
+        caneta.escrever(); //Utilizando o método.
+        
+    }
+}
